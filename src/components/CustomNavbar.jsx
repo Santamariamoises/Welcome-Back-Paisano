df7c69e82724be59c2c66ed55a0039921bd35577
santamariamoises <santamaria.moises@gmail.com>
2018-09-17 13:58:26 -0500

    cleaning up code

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -6,7 +6,8 @@ import './CustomNavbar.css'
 
 export default class CustomNavbar extends Component {
   render() {
-    return (<Navbar default="default" collapseOnSelect="collapseOnSelect">
+    return (
+      <Navbar default="default" collapseOnSelect="collapseOnSelect">
       <Navbar.Header>
         <Navbar.Brand>
           <Link to="/">CODERS MOST WANTED!!</Link>
@@ -15,18 +16,10 @@ export default class CustomNavbar extends Component {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight="pullRight">
-          <NavItem eventKey={1} componentClass={Link} href="/" to="/">
-            Home
-          </NavItem>
-          <NavItem eventKey={3} componentClass={Link} href="/resources" to="/resources">
-            Resources
-          </NavItem>
-          <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">
-            Know Your Rights
-          </NavItem>
-          <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">
-            Team
-          </NavItem>
+          <NavItem eventKey={1} componentClass={Link} href="/" to="/">Home</NavItem>
+          <NavItem eventKey={3} componentClass={Link} href="/resources" to="/resources">resources</NavItem>
+          <NavItem eventKey={3} componentClass={Link} href="/news" to="/news">Know Your Rights</NavItem>
+          <NavItem eventKey={2} componentClass={Link} href="/about" to="/about">Team</NavItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,23 +7,22 @@ import ChatBot from 'react-simple-chatbot';
 export default class Home
  extends Component {
   render() {
-    return (<div>
-
-      <Grid>
-
-      <Jumbotron>
-      <h2>Welcome Back Paisano</h2>
-      <Image src="assets/welcomemex.jpg" className="about-profile-picmex" rounded="rounded"/>
-          <p><b> Helping Returnees And Deportees Start Over</b></p>
-          <p><b>Help us by answering some questions!</b></p>
-          <Link to="/learnMore">
+    return (
+      <div>
+        <Grid>
+          <Jumbotron>
+            <h2>Welcome Back Paisano</h2>
+            <Image src="assets/welcomemex.jpg" className="about-profile-picmex" rounded="rounded"/>
+            <p><b> Helping Returnees And Deportees Start Over</b></p>
+            <p><b>Help us by answering some questions!</b></p>
+            <Link to="/learnMore">
             <Button bsStyle="primary">Questionary</Button>
-          </Link>
-        </Jumbotron>
-        <Image src="assets/amdream.jpg" className="about-profile-pic" rounded="rounded"/>
-        <Image src="assets/ice.jpg" className="about-profile-pic" rounded="rounded"/>
-        <Image src="assets/iceplane.jpg" className="about-profile-pic1" rounded="rounded"/>
-      </Grid>
-    </div>)
+            </Link>
+          </Jumbotron>
+            <Image src="assets/amdream.jpg" className="about-profile-pic" rounded="rounded"/>
+            <Image src="assets/ice.jpg" className="about-profile-pic" rounded="rounded"/>
+            <Image src="assets/iceplane.jpg" className="about-profile-pic1" rounded="rounded"/>
+        </Grid>
+      </div>)
   }
 }
